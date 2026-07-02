8edafa38fd9ea6a38a87f78fd2b1dc7bcf13dd67
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2026-07-03 01:33:04 +0300

    feat: colors in lua

---
+++ lua/zx/highlight.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_set_hl(0, "zxbLineNumber", {
+	fg = "#505050",
+})
+
+vim.api.nvim_set_hl(0, "zxbComment", {
+	fg = "#666666",
+	italic = true,
+})
+
+vim.api.nvim_set_hl(0, "zxbGraphics", {
+	fg = "#61afef",
+})
+
+vim.api.nvim_set_hl(0, "zxbColour", {
+	fg = "#c678dd",
+})
