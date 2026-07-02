02e199ebcf3b5fea0e393ac0163ef8025457554b
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2026-07-03 01:41:39 +0300

    fix: use ctermfg

--- lua/zx/highlight.lua
+++ lua/zx/highlight.lua
@@ -1,16 +1,16 @@
-vim.api.nvim_set_hl(0, "zxbLineNumber", {
-	fg = "#505050",
-})
+local function hi(group, gui, cterm, opts)
+	opts = opts or {}
+	vim.cmd(string.format(
+		"highlight %s guifg=%s ctermfg=%d%s%s",
+		group,
+		gui,
+		cterm,
+		opts.italic and " gui=italic" or "",
+		opts.italic and " cterm=italic" or ""
+	))
+end
 
-vim.api.nvim_set_hl(0, "zxbComment", {
-	fg = "#0000ff",
-	italic = true,
-})
-
-vim.api.nvim_set_hl(0, "zxbGraphics", {
-	fg = "#61afef",
-})
-
-vim.api.nvim_set_hl(0, "zxbColour", {
-	fg = "#c678dd",
-})
+hi("zxbComment", "#666666", 244, { italic = true })
+hi("zxbLineNumber", "#505050", 239)
+hi("zxbGraphics", "#61afef", 75)
+hi("zxbColour", "#c678dd", 176)
